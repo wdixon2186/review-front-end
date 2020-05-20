2797471d3cc6df9fdd07dab166e44b12e32c7921
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-05-19 22:25:45 -0600

    started the front end

---
+++ reviews/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the start now boi!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+//Header. 
+//Most recent review
+//Backlog on a seperate page. 
+//About on a seperate page. 
